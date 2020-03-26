7fb174714846b721684a39ad6f553c1a13cbf812
Juan Suman <victor.suman23@gmail.com>
2020-03-26 19:17:44 -0300

    Projeto criado

---
+++ minichallenge02/minichallenge02/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  minichallenge02
+//
+//  Created by Juan Suman on 26/03/20.
+//  Copyright © 2020 Juan Suman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ minichallenge02/minichallenge02Tests/minichallenge02Tests.swift
@@ -0,0 +1,34 @@
+//
+//  minichallenge02Tests.swift
+//  minichallenge02Tests
+//
+//  Created by Juan Suman on 26/03/20.
+//  Copyright © 2020 Juan Suman. All rights reserved.
+//
+
+import XCTest
+@testable import minichallenge02
+
+class minichallenge02Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}

61cabe5263f1cdfd2987184f4fcf153149deed91
Juan Suman <victor.suman23@gmail.com>
2020-04-06 14:39:34 -0300

    Revert "Telas de cadastro"
    This reverts commit c3ab9a091dab5c97bb11e21b7a42bc37e364f56f.

--- minichallenge02/minichallenge02/ViewController.swift
+++ minichallenge02/minichallenge02/ViewController.swift
@@ -9,32 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-<<<<<<< Updated upstream
     
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var actionButton: UIButton!
-=======
-
-    @IBOutlet weak var nomeLabel: UILabel!
-    
-    @IBOutlet weak var nomeDaCriancaTextField: UITextField!
-   
-    @IBAction func avancarAction(_ sender: Any) {
-        let nome = nomeDaCriancaTextField.text
-        
-        UserDefaults.standard.set(nome, forKey: "nome")
-        
-        performSegue(withIdentifier: "deNomePraAvatar", sender: self)
-    }
->>>>>>> Stashed changes
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-//        guard let nomeAnterior = UserDefaults.standard.value(forKey: "nome") else { return }
-        
-//        nomeLabel.text = nomeAnterior as? String
-        
+        // Do any additional setup after loading the view.
     }
 
     @IBAction func changeText(_ sender: Any) {

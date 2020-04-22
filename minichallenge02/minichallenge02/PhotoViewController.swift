6889a2afccea16a0a742490b0cdbc29e4e94d9da
Juan Suman <victor.suman23@gmail.com>
2020-04-22 18:38:11 -0300

    implementando contato com imageView

--- minichallenge02/minichallenge02/PhotoViewController.swift
+++ minichallenge02/minichallenge02/PhotoViewController.swift
@@ -17,20 +17,17 @@ class PhotoViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageTapped(tapGestureRecognizer:)))
+        imageView.isUserInteractionEnabled = true
+        imageView.addGestureRecognizer(tapGestureRecognizer)
+        
         //Recebe a foto passada da outra tela.
         imageView.image = photo
-
-        // Do any additional setup after loading the view.
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func imageTapped(tapGestureRecognizer: UITapGestureRecognizer){
+        performSegue(withIdentifier: "homeSegue", sender: self)
+        
     }
-    */
 
 }
